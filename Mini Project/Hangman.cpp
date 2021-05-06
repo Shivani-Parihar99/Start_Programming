539ca25892111ee3473da4aefe59842c9296add7
Shivani-Parihar99 <81503636+Shivani-Parihar99@users.noreply.github.com>
2021-05-06 15:21:17 +0530

    Update Hangman.cpp

--- Mini Project/Hangman.cpp
+++ Mini Project/Hangman.cpp
@@ -15,3 +15,19 @@ void replaceDashes(char guessWord[], int length);
 void displayWord(string word, int length);
 int isGuessTrue(string secretWord, char guessWord[], char letter);
 void displayMan(int remainingGuess);
+int main() {
+    int maxTries = 5;
+    int remainingTries = 5;
+    char guessLetter;
+    string secretWord;
+    int secretWordLength;
+
+    displayGameDetails(maxTries);
+ 
+    secretWord = chooseSecretWord();
+    secretWordLength = secretWord.length();
+
+    char guessWord[secretWordLength];
+    replaceDashes(guessWord, secretWordLength);
+    cout << "Your guess word is:";
+    displayWord(guessWord, secretWordLength);
