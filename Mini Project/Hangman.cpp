4b3cbdd2cc3afa5ef1eba9fcd5d51be506a3b469
Shivani-Parihar99 <81503636+Shivani-Parihar99@users.noreply.github.com>
2021-05-06 16:41:07 +0530

    Update Hangman.cpp

--- Mini Project/Hangman.cpp
+++ Mini Project/Hangman.cpp
@@ -31,3 +31,39 @@ int main() {
     replaceDashes(guessWord, secretWordLength);
     cout << "Your guess word is:";
     displayWord(guessWord, secretWordLength);
+    while (remainingTries != 0) {
+
+        cout << "Enter your guess letter:" << endl;
+        cin >> guessLetter;
+
+        int guess = isGuessTrue(secretWord, guessWord, guessLetter);
+
+        if (guess == 0) {
+            remainingTries--;
+            cout << "\nWhoops! that letter is not present in the word" << endl;
+            displayMan(remainingTries);
+        }
+        if (guess == 1) {
+            cout << "\nYay! You have found the letter" << endl;
+        }
+        if (guess == 2) {
+            cout << "\nYou have already guessed this letter. Try something else!" << endl;
+        }
+        cout << "You can have " << remainingTries << " more wrong attempts" << endl;
+        cout << "Your guess word is:";
+        displayWord(guessWord, secretWordLength);
+        cout << endl;
+
+        if (secretWord == guessWord) {
+            cout << "\nCongratulation! You won." << endl;
+            return 0;
+        }
+
+    }
+    if (secretWord != guessWord){
+    cout << "\nToo many Guesses! You have been hanged." << endl;
+    cout << "\nThe secret word was: ";
+    displayWord(secretWord, secretWordLength);
+    }
+}
+
