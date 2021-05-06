e48c79ad3a5fcdf6016ca7ca81de342bf7288e08
Shivani-Parihar99 <81503636+Shivani-Parihar99@users.noreply.github.com>
2021-05-06 15:19:54 +0530

    Create Hangman.cpp

---
+++ Mini Project/Hangman.cpp
@@ -0,0 +1,17 @@
+// Hangman Game 
+#include<iostream>
+
+#include<cstdlib>
+
+#include<ctime>
+
+#include <string>
+
+using namespace std;
+
+void displayGameDetails(int maxTries);
+string chooseSecretWord();
+void replaceDashes(char guessWord[], int length);
+void displayWord(string word, int length);
+int isGuessTrue(string secretWord, char guessWord[], char letter);
+void displayMan(int remainingGuess);
